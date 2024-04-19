6353858059db4bcdbd2dec637415b2d64fcea83a
cyberbyte53 <namansinghrana53@gmail.com>
2024-04-19 22:20:32 +0530

    cors error handled

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 from getSequence import getseq
 import os
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/process_data", methods=["POST"])
